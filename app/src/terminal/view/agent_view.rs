warpdotdev/warp

Properly escape cd during conversation restoration, and don't cd for non-local conversations. (#25383)
## Description
<!-- Please remember to add your design buddy onto the PR for review, if
it contains any UI changes! -->
This PR addresses [this
vulnerability](https://github.com/warpdotdev/warp/security/advisories/GHSA-8659-m852-gmfx)
relating to `cd` command quoting for conversation restoration. We use
the existing `.shell_family().shell_escape()` method for shell escaping
that we use throughout the app.

We also add a new `RestorationDirState` enum option,
`SkippedNonLocalConversation`, for conversations that we don't have
local data for. This used to be caught by the `MissingOriginalDir`
variant when we checked whether the target dir existed on the local
machine, but in a world where an attacker could create an AIConversation
object that had a working dir that resolved to a valid path on your
machine, this would make it theoretically possible to execute an
arbitrary `cd` to be run into a directory on your machine.

Since we know that conversations without local data are being synced
from the cloud or from another machine, we can skip the directory
check—it doesn't make sense to try and opportunistically `cd` into a
directory here.

## Testing
<!--
How did you test this change? What automated tests did you add? If you
didn't add any new tests, what's your justification for not adding any?
-->
Manual testing:
- [x] Loaded in old AI conversation, ensured that we do cd
- [x] Loaded in team member conversation, no cd
- [x] Closed and reopened Warp, checked on conversation restoration

## Agent Mode
- [x] Warp Agent Mode - This PR was created via Warp's AI Agent Mode

app/src/terminal/view/agent_view.rs
@@ -212,10 +212,15 @@ impl TerminalView {
                     }
                     Box::new(|_, _| {})
                 };
+                let is_local = BlocklistAIHistoryModel::handle(ctx)
+                    .as_ref(ctx)
+                    .get_conversation_metadata(&conversation_id)
+                    .is_some_and(|m| m.has_local_data);
                 me.restore_conversation_and_directory_context(
                     conversation,
                     false,
                     RestoreConversationEntryBehavior::PreserveAgentViewState,
+                    is_local,
                     on_restored,
                     ctx,
                 );

app/src/workspace/view.rs
@@ -12641,6 +12641,10 @@ impl Workspace {
         }
 
         let history_model = BlocklistAIHistoryModel::handle(ctx);
+        let is_local_conversation = history_model
+            .as_ref(ctx)
+            .get_conversation_metadata(&conversation_id)
+            .is_some_and(|m| m.has_local_data);
         let future = history_model
             .as_ref(ctx)
             .load_conversation_data(conversation_id, ctx);
@@ -12674,6 +12678,7 @@ impl Workspace {
                     conversation,
                     FeatureFlag::AgentView.is_enabled(),
                     RestoreConversationEntryBehavior::PreserveAgentViewState,
+                    is_local_conversation,
                     move |terminal_view, ctx| {
                         terminal_view.enter_agent_view_for_conversation(
                             None,

chenchangzhong/zap

(cherry picked from commit c697c8f5f62ba83ca01154e8ae6940d1e7bd29d1)

app/src/terminal/view/agent_view.rs
@@ -146,9 +146,14 @@ impl TerminalView {
                 } else {
                     Box::new(|_, _| {})
                 };
+                let is_local = BlocklistAIHistoryModel::handle(ctx)
+                    .as_ref(ctx)
+                    .get_conversation_metadata(&conversation_id)
+                    .is_some_and(|m| m.is_restorable_locally);
                 me.restore_conversation_and_directory_context(
                     conversation,
                     false,
+                    is_local,
                     on_restored,
                     ctx,
                 );

app/src/workspace/view.rs
@@ -11009,6 +11009,10 @@ impl Workspace {
         }
 
         let history_model = BlocklistAIHistoryModel::handle(ctx);
+        let is_local_conversation = history_model
+            .as_ref(ctx)
+            .get_conversation_metadata(&conversation_id)
+            .is_some_and(|m| m.is_restorable_locally);
         let future = history_model
             .as_ref(ctx)
             .load_conversation_data(conversation_id);
@@ -11043,6 +11047,7 @@ impl Workspace {
                 terminal_view.restore_conversation_and_directory_context(
                     conversation,
                     FeatureFlag::AgentView.is_enabled(),
+                    is_local_conversation,
                     |terminal_view, ctx| {
                         terminal_view.redetermine_global_focus(ctx);
                     },